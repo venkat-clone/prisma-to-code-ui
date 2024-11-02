d64fbfad5127e7b8a7bd48d2848723a5ec5bb432
venkey <lingampally.venkey@gmail.com>
2024-11-02 14:03:31 +0530

    dirctory creation dynamically

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,24 @@ const fs = require('fs');
 const Queue = require('bull');
 
 const router = express.Router();
+const UPLOAD_DIR = 'tmp/uploads';
+if (!fs.existsSync(UPLOAD_DIR)) {
+  fs.mkdirSync(UPLOAD_DIR, { recursive: true });
+}
+
+const OUTPUT_DIR = 'tmp/schemas';
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR, { recursive: true });
+}
 
 // Set up a job queue
 const codeGenerationQueue = new Queue('code generation', {
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD,
-    tls: {} // Required by Upstash
-  }
+  // redis: {
+  //   host: process.env.REDIS_HOST,
+  //   port: process.env.REDIS_PORT,
+  //   password: process.env.REDIS_PASSWORD,
+  //   tls: {} // Required by Upstash
+  // }
 });
 
 // Job processor
@@ -29,7 +38,7 @@ codeGenerationQueue.process(async (job) => {
     await runCodeGeneration(filePath, outputFileName);
 
     // Create a ZIP file
-    const zipFilePath = path.join('tmp', `${path.basename(outputFileName)}.zip`);
+    const zipFilePath = path.join(OUTPUT_DIR, `${path.basename(outputFileName)}.zip`);
     const output = fs.createWriteStream(zipFilePath);
     const archive = archiver('zip', { zlib: { level: 9 } });
 
@@ -47,9 +56,11 @@ codeGenerationQueue.process(async (job) => {
   }// Return the path of the created zip
 });
 
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Directory where files will be stored
+    cb(null, UPLOAD_DIR); // Directory where files will be stored
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`); // Create a unique filename
@@ -78,19 +89,20 @@ router.post('/generate-code', upload.single('schema'), async (req, res, next) =>
     if (!req.file) {
       return res.status(400).send('No file uploaded.');
     }
+    console.log(req.file);
 
     // Add a job to the queue
     const job = await codeGenerationQueue.add({
       filePath: req.file.path,
-      outputFileName: path.join('tmp', req.file.filename)
+      outputFileName: path.join(OUTPUT_DIR, req.file.filename)
     });
 
     // Respond immediately with job details or an identifier
     // res.status(202).json({ message: 'Job is being processed', jobId: job.id });
-    res.render('/', { title: 'Express', jobId: job.id });
+    res.render('index', { title: 'Express', jobId: job.id });
   } catch (error) {
     console.log(error);
-    res.render('/', { title: 'Express', error: error.message });
+    res.render('index', { title: 'Express', error: error.message });
   }
 });
 
